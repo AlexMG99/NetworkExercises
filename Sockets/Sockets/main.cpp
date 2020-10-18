71667728f7ee80b3a015b92a1d802ab0e40613c6
Alex Morales Garcia <36154498+AlexMG99@users.noreply.github.com>
2020-10-18 19:45:04 +0200

    UDP finished kind of

--- Sockets/Sockets/main.cpp
+++ Sockets/Sockets/main.cpp
@@ -5,6 +5,11 @@ int main(int argc, char** argv)
 	InitSockets();
 	int port = 8000;
 	SOCKET s = socket(AF_INET, SOCK_DGRAM, 0);
+	const char* msg = "ping";
+	char msg_recieved;
+	int res = 0;
+
+	printf("Esto es el cliente UDP!\n");
 
 	sockaddr_in bindAddr;
 	bindAddr.sin_family = AF_INET;
@@ -13,8 +18,27 @@ int main(int argc, char** argv)
 	sockaddr_in remoteAddr;
 	remoteAddr.sin_family = AF_INET;
 	remoteAddr.sin_port = htons(port);
-	const char* remoteAddrStr = "192.168.0.1";
+	const char* remoteAddrStr = "127.0.0.1";
 	inet_pton(AF_INET, remoteAddrStr, &remoteAddr.sin_addr);
+
+	while (true)
+	{
+		// Send message to server
+		res = sendto(s, msg, sizeof(msg), 0, (sockaddr*)(&remoteAddr), sizeof(remoteAddr));
+
+		// Recieve message from server
+		int size = sizeof(bindAddr);
+		res = recvfrom(s, &msg_recieved, sizeof(char*), 0, (sockaddr*)&bindAddr, &size);
+
+		if (res == SOCKET_ERROR)
+			printWSErrorAndExit("Message not recieved!");
+		else
+		{
+			printf_s(&msg_recieved);
+			printf_s("\n");
+			Sleep(1000);
+		}
+	}
 
 	closesocket(s);
 	system("pause");
--- Sockets/UDP Server/main.cpp
+++ Sockets/UDP Server/main.cpp
@@ -3,11 +3,47 @@
 int main(int argc, char** argv)
 {
 	InitSockets();
+	int res = 0;
 	int port = 8000;
 	SOCKET s = socket(AF_INET, SOCK_DGRAM, 0);
+	const char* msg = "pong";
+	char msg_recieved;
+
+	printf("Esto es el server UDP!\n");
+
 	sockaddr_in bindAddr;
 	bindAddr.sin_family = AF_INET;
 	bindAddr.sin_port = htons(port);
+	bindAddr.sin_addr.S_un.S_addr = INADDR_ANY;
+
+	res = bind(s, (const struct sockaddr*)&bindAddr, sizeof(bindAddr));
+
+	if (res == SOCKET_ERROR) 
+	{
+		printWSErrorAndExit("Socket error! Not binded correctly");
+	}
+
+	while(true)
+	{
+		// Recieve message from client
+		int size = sizeof(bindAddr);
+		res = recvfrom(s, &msg_recieved, sizeof(char*), 0, (sockaddr*)&bindAddr, &size);
+
+		if (res == SOCKET_ERROR)
+			printWSErrorAndExit("Message not recieved!");
+		else
+		{
+			printf_s(&msg_recieved);
+			printf_s("\n");
+			Sleep(1000);
+		}
+
+		// Send message to client
+		res = sendto(s, msg, sizeof(msg), 0, (sockaddr*)(&bindAddr), sizeof(bindAddr));
+		if (res == SOCKET_ERROR)
+			printWSErrorAndExit("Message don't send!");
+	}
+	
 
 	closesocket(s);
 	system("pause");
